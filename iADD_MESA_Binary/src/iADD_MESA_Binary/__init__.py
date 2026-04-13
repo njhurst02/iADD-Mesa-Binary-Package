7a9fcdbad82a89cea87ead99b9954d0a68bf165b
njhurst02 <nicholashurst02@gmail.com>
2026-04-13 15:19:30 -0500

    Add files via upload

---
+++ iADD_MESA_Binary/src/iADD_MESA_Binary/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+from .iadd_mesa_binary import (
+	plot_Mass_Transfer,
+	plot_Roche_Lobe,
+	plot_Hertzsprung_Russel,
+	plot_Abundances,
+	plot_Kippenhahn,
+	get_Binary_Data,
+	get_Data,
+)
+
+__version__ = "0.1.0"
+__author__ = "Nicholas Hurst"
+__email__ = "njhurst2@illinois.edu"
